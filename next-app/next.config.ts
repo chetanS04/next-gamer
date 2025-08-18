96faf4c1bbab415178fded601b7b21c9162fc679
chetans04 <santlal4147@gmail.com>
2025-08-18 16:06:59 +0530

    change url

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,19 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "www.api.idbazaar.topntech.com",
-//         port: "",
-//         pathname: "/storage/**",
-//       },
-//     ],
-//   },
-// };
 
-// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
